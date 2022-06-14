eec176a18018ff139fa35fa1c4275cf132e60552
Lili <lisaroure@gmail.com>
2022-06-14 13:48:45 +0200

    add new files

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,16 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://piiquante:qBXn3B5M5SCONjNe@cluster0.cdgq4dw.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 app.use((req, res, next) => {
     console.log('Requête reçue !');
     next();
