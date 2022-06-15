ad13f8030f0136ccb17f32a9546c46e8ba788e4c
Lili <lisaroure@gmail.com>
2022-06-15 17:34:04 +0200

    updt

--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-// in routes/stuff.js
+const auth = require('../middleware/auth');
 
 const stuffCtrl = require('../controllers/stuff');
 
-router.get('/', stuffCtrl.getAllStuff);
-router.post('/', stuffCtrl.createThing);
-router.get('/:id', stuffCtrl.getOneThing);
-router.put('/:id', stuffCtrl.modifyThing);
-router.delete('/:id', stuffCtrl.deleteThing);
+router.get('/', auth, stuffCtrl.getAllStuff);
+router.post('/', auth, stuffCtrl.createThing);
 
-module.exports = router;
+module.exports = router; 
