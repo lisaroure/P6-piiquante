159d4121416eff97f4740fc563b73ac97dd8d122
Lili <lisaroure@gmail.com>
2022-06-28 11:58:00 +0200

    correction token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -34,7 +34,7 @@ exports.login = (req, res, next) => {
                         userId: user._id,
                         token: jwt.sign(
                             { userId: user._id },
-                            'udl*VFMnxp5Crly-({',
+                            'RANDOM_TOKEN_SECRET',
                             { expiresIn: '24h' }
                         )
                     });
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ const user = require('../models/user');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'udl*VFMnxp5Crly-({');
+        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
         req.auth = { userId: userId };
         if (req.body.userId && req.body.userId !== userId) {
